f2a4ab12ce389541fe3134c8940bc09af8bb270b
Anthony Dougherty <adougherty@escspectrum.com>
2024-08-16 22:44:52 -0400

    Add project files.

---
+++ Class-3/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿// MainWindow.xaml.cs
+using System.Windows;
+
+namespace ProjectApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button 1 Clicked!");
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button 2 Clicked!");
+        }
+    }
+}
