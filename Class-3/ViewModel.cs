38cebb295535d064c9167409463dfb18104ea1aa
Basic-beep <elliottroseemail@gmail.com>
2024-09-09 20:40:36 -0400

    week 4  class

--- Class-3/ViewModel.cs
+++ Class-3/ViewModel.cs
@@ -1,11 +1,13 @@
 ﻿// ViewModel.cs
 using System.ComponentModel;
+using System.Windows.Input;
 
 namespace ProjectApp
 {
     public class ViewModel : INotifyPropertyChanged
     {
         private string _userInput;
+
         public string UserInput
         {
             get { return _userInput; }
@@ -19,10 +21,50 @@ namespace ProjectApp
             }
         }
 
+        private string _userInputReversed;
+
+        public string UserInputReversed
+        {
+            get { return _userInputReversed; }
+            set
+            {
+                if (_userInputReversed != value)
+                {
+                    _userInputReversed = value;
+                    OnPropertyChanged(nameof(UserInputReversed));
+                }
+            }
+        }
+
+        public ViewModel()
+        {
+            ButtonClicked = new RelayCommand(buttonclicked, canclickbutton);
+        }
+
+
+        private bool canclickbutton(object value)
+        {
+            return !string.IsNullOrEmpty(_userInput);
+        }
+
+        private void buttonclicked(object value)
+        {
+            char[] charArray = UserInput.ToCharArray();
+            Array.Reverse(charArray);
+
+            UserInputReversed = new string(charArray);
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        public ICommand ButtonClicked {  get; set; }
+
+
+
+
     }
 }
