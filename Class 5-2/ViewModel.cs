99f0aaa81555570cd7d493bc7a523fe0c1b7768a
ane0phyte <63813373+ane0phyte@users.noreply.github.com>
2024-09-22 18:24:47 -0400

    Class 5 Updates

---
+++ Class 5-2/ViewModel.cs
@@ -0,0 +1,30 @@
+﻿// ViewModel.cs
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Class_5_2
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<string> _items;
+        public ObservableCollection<string> FilteredItems { get; set; }
+
+        public ViewModel()
+        {
+            _items = new ObservableCollection<string>
+            {
+                "Apple",
+                "Banana",
+                "Cherry"
+            };
+            FilteredItems = new ObservableCollection<string>(_items.Where(i => i.StartsWith("A")));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Class 5/ViewModel.cs
@@ -0,0 +1,27 @@
+﻿// ViewModel.cs
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Class_5
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<string> Items { get; set; }
+
+        public ViewModel()
+        {
+            Items = new ObservableCollection<string>
+            {
+                "Item 1",
+                "Item 2",
+                "Item 3"
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
