923d186ce7b2413f8327b129a3a2010809299c08
CT7 <83499017+CT7-labs@users.noreply.github.com>
2024-09-30 14:14:02 -0400

    merge from prestons stuff

--- Class 5-2/ViewModel.cs
+++ Class 5-2/ViewModel.cs
@@ -3,31 +3,13 @@ using Microsoft.VisualBasic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.Windows.Data;
+using System.Windows.Controls;
 
 namespace Class_5_2
 {
     public class ViewModel : INotifyPropertyChanged
     {
-        private string _filterText;
-
-
-        public string FilterText
-        {
-            get
-            {
-                return _filterText;
-            }
-            set
-            {
-                if (_filterText != value)
-                {
-                    _filterText = value;
-                    UpdateFilteredItems();
-                }
-            }
-        }
-
+        // Collection of items setup
         private ObservableCollection<string> _items;
         private ObservableCollection<string> _filteredItems;
         public ObservableCollection<string> FilteredItems
@@ -52,16 +34,96 @@ namespace Class_5_2
             }
         }
 
+        // User input setup
+        private String _userInput;
+        public String UserInput
+        {
+            get { return _userInput; }
+
+            set
+            {
+                if (_userInput != value)
+                {
+                    _userInput = value;
+                    OnPropertyChanged(nameof(UserInput));
+
+                    FilterItems(); // filter items after user input is updated
+                }
+            }
+        }
+
+        // Case-sensitive checkbox setup
+        private bool _isCaseSensitive;
+        public bool IsCaseSensitive
+        {
+            get { return _isCaseSensitive;}
+            set
+            {
+                if (_isCaseSensitive != value)
+                {
+                    _isCaseSensitive = value;
+                    OnPropertyChanged(nameof(IsCaseSensitive));
+
+                    FilterItems();
+                }   
+            }
+        }
         public ViewModel()
         {
             _items = new ObservableCollection<string>
             {
                 "Apple",
                 "Banana",
-                "Cherry"
+                "Cherry",
+                "Date",
+                "Eggplant",
+                "Fig",
+                "Grape",
+                "Honeydew",
+                "Iceberg lettuce",
+                "Jackfruit",
+                "Kiwi",
+                "Lemon",
+                "Mango",
+                "Nectarine",
+                "Orange",
+                "Papaya",
+                "Quince",
+                "Raspberry",
+                "Strawberry",
+                "Tomato",
+                "Ugli fruit",
+                "Vanilla bean",
+                "Watermelon",
+                "Xigua",
+                "Yam",
+                "Zucchini"
             };
-            FilteredItems = new ObservableCollection<string>(_items);
 
+            // Filter items at beginning of program; which isn't necessary, but a nice-to-have
+            FilterItems();
+        }
+
+        public void FilterItems()
+        {
+            if (UserInput != null)
+            {
+                if (IsCaseSensitive)
+                {
+                    // Case-sensitive filtering
+                    FilteredItems = new ObservableCollection<string>(_items.Where(i => i.Contains(UserInput)));
+                }
+                else
+                {
+                    // Case-insensitive filtering
+                    FilteredItems = new ObservableCollection<string>(_items.Where(i => i.Contains(UserInput, StringComparison.OrdinalIgnoreCase)));
+                }
+            }
+            else
+            {
+                FilteredItems = new ObservableCollection<string>(_items);
+            }
+            OnPropertyChanged(nameof(FilteredItems));
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
