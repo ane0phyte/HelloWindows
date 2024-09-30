c5a48ef90c7060fc1a9c6b046e8e5020d1450cad
ane0phyte <63813373+ane0phyte@users.noreply.github.com>
2024-09-29 21:12:41 -0400

    Class 6 updates

--- Class 5-2/ViewModel.cs
+++ Class 5-2/ViewModel.cs
@@ -1,14 +1,56 @@
 ﻿// ViewModel.cs
+using Microsoft.VisualBasic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Windows.Data;
 
 namespace Class_5_2
 {
     public class ViewModel : INotifyPropertyChanged
     {
+        private string _filterText;
+
+
+        public string FilterText
+        {
+            get
+            {
+                return _filterText;
+            }
+            set
+            {
+                if (_filterText != value)
+                {
+                    _filterText = value;
+                    UpdateFilteredItems();
+                }
+            }
+        }
+
         private ObservableCollection<string> _items;
-        public ObservableCollection<string> FilteredItems { get; set; }
+        private ObservableCollection<string> _filteredItems;
+        public ObservableCollection<string> FilteredItems
+        {
+            get { return _filteredItems; }
+            set
+            {
+                _filteredItems = value;
+                OnPropertyChanged(nameof(FilteredItems));
+            }
+        }
+
+        private void UpdateFilteredItems()
+        {
+            if (_items == null || String.IsNullOrEmpty(_filterText))
+            {
+                FilteredItems = new ObservableCollection<string>(_items);
+            }
+            else
+            {
+                FilteredItems = new ObservableCollection<string>(_items.Where(i => i.ToUpper().Contains(_filterText.ToUpper())));
+            }
+        }
 
         public ViewModel()
         {
@@ -18,7 +60,8 @@ namespace Class_5_2
                 "Banana",
                 "Cherry"
             };
-            FilteredItems = new ObservableCollection<string>(_items.Where(i => i.StartsWith("A")));
+            FilteredItems = new ObservableCollection<string>(_items);
+
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
