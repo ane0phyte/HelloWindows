ee96eeafd0645b6f3287fc9bd9f7979eb0d16aef
ane0phyte <63813373+ane0phyte@users.noreply.github.com>
2024-10-13 17:01:39 -0400

    Class 7
    Generics

---
+++ Class_7/ClassSevenViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Class_7
+{
+    public class ClassSevenViewModel
+    {
+        public ClassSevenViewModel()
+        {
+            ObservableCollection<string> items = new ObservableCollection<string> { "Apple", "Banana", "Cherry" };
+            var firstItem = Helpers.GetFirst(items);
+            MessageBox.Show($"First item: {firstItem}");
+            Application.Current.Shutdown(1);
+
+        }
+    }
+}
---
+++ Class_7/Helpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_7
+{
+    public static class Helpers
+    {
+        public static T? GetFirst<T>(ObservableCollection<T> list)
+        {
+            return list.Count > 0 ? list[0] : default;
+        }
+    }
+
+}
